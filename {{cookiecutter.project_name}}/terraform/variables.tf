e0ebd5dcbfa1ac57d166fdd907add940b2854513
arunpyx <arun@pyx.is>
2023-09-13 12:49:41 +0530

    variables updated

--- {{cookiecutter.project_name}}/terraform/variables.tf
+++ {{cookiecutter.project_name}}/terraform/variables.tf
@@ -1,16 +1,17 @@
 # Used to tag resources
 variable "env" { default = "terraform" }
 variable "aws_region"      { default = "{{cookiecutter.aws_region}}" }
-variable "aws_subregion_a" { default = "us-east-2a" }
-variable "instance_ami"  { default = "ami-06c7d6c0987eaa46c" }
-variable "aws_access_key" { default = "ASIASNUU6P2WT5BF674Q" }
-variable "aws_secret_key" { default = "mbKIkqLxGISvmOq+YWAiGjwPgdVsSkuJQ8/GXAmH" }
-variable "pyxelchain-domain" { default = "pyxelchain-tf.gameficap.com" }
+variable "aws_subregion_a" { default = "{{cookiecutter.availability_zone}}" }
+variable "instance_ami"  { default = "{{cookiecutter.instance_ami}}" }
+variable "aws_access_key" { default = "{{cookiecutter.aws_access_key}}" }
+variable "aws_secret_key" { default = "{{cookiecutter.aws_secret_key}}" }
+variable "pyxelchain-domain" { default = "{{cookiecutter.pyxelchain-domain}}" }
+
 
 variable "region" {
   description = "AWS region to manage resources in"
   type        = string
-  default     = "us-east-2" # Just for dev
+  default     = "{{cookiecutter.aws_region}}" # Just for dev
 }
 
 variable "instance_name" {
@@ -22,7 +23,7 @@ variable "instance_name" {
 variable "instance_type" {
   description = "EC2 instance type tag e.g. t2.micro"
   type        = string
-  default     = "t3.medium" # t3.xlarge?
+  default     = "{{cookiecutter.instance_type}}" # t3.xlarge?
 }
 
 variable "subnet_id" {}
@@ -30,5 +31,5 @@ variable "subnet_id" {}
 variable "ami" {
   description = "EC2 instance image to use - per region. See https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/finding-an-ami.html"
   type        = string
-  default     = "ami-0df2643dffb0fed91"
+  default     = "{{cookiecutter.ami}}"
 }
