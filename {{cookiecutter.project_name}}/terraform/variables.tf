aa6ac817787535e6f70f310b09e338aed9ecdb2f
arunpyx <arun@pyx.is>
2023-09-13 13:01:26 +0530

    test9

--- {{cookiecutter.project_name}}/terraform/variables.tf
+++ {{cookiecutter.project_name}}/terraform/variables.tf
@@ -5,4 +5,4 @@ variable "aws_subregion_a" { default = "{{cookiecutter.availability_zone}}" }
 variable "instance_ami"  { default = "{{cookiecutter.instance_ami}}" }
 variable "aws_access_key" { default = "{{cookiecutter.aws_access_key}}" }
 variable "aws_secret_key" { default = "{{cookiecutter.aws_secret_key}}" }
-variable "pyxelchain-domain" { default = "{{cookiecutter.pyxelchain-domain}}" }
+variable "pyxelchain-domain" { default = "{{cookiecutter.domain_name}}" }
